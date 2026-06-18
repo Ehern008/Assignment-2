1c44c1c1eb9bcfd6dc3dfc2f846c75f1f960f9b7
Ehern008 <ehern008@gmail.com>
2026-06-17 19:12:28 -0700

    Add files via upload

---
+++ COMP 648/index.html/script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bare Bones JS</title>
+</head>
+<body>
+
+    <h1>Open your console to see the output!</h1>
+
+    <!-- Link to external JavaScript file -->
+    <script src="index.html"></script>
+</body>
+</html>
---
+++ COMP 648/js/script.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bare Bones JS</title>
+</head>
+<body>
+
+    <h1>Open your console to see the output!</h1>
+
+    <!-- Link to external JavaScript file -->
+    <script src="index.html"></script>
+</body>
+</html>
+
+// Exercises due 6/17/2026
+
+1.
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+2. 
+5.5
+let "stringLiteralExpression";
+let trueValue = true;
+let nullValue = null;
+
+3. 
+let price = 10; 
+let total = price * 2;
+
+4.
+let sfirstName;
+let sLastName;
+let sAddress;
+let sCity;
+let sState;
+let sZipCode;
+let intYourAge;
+let sReferralSource;
+let bMayWeContactYou;
+
+5.
+let sState; const sState; var sState;
+let iZipCode; const iZipCode; var iZipCode;
+let iYourAge; const iYourAge; var iYourAge;
+
+6.
+let sScore = 90;
+let sResult = `Your score is ${sScore}.`;
+console.log(sResult);
+
+7.
+//step 1: Create two variables.
+
+let ace = 5;
+let bee = true;
+
+console.log(ace + bee);
